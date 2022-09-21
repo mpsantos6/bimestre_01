7040b13e11eb3c7ff9a8a51522720f2221e9261d
mpcroiss <marcospaulo.es19@gmail.com>
2022-09-21 19:41:14 -0300

    atividade_02

---
+++ atividade_02/src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Dados from './Componentes/Dados'
+
+export default function App(){
+  return(
+    <>
+     <Dados/>
+    </>
+  )
+}
---
+++ atividade_02/src/Componentes/Dados.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Dados (){
+    return (
+        <section>
+            <p>Data: 2 de Agosto de 2019 (Sexta Feira)</p>
+            <p>Horario: 18h30 - 00h00</p>
+            <p>Local: Parque do Povo</p>
+            <p>Cidade: Campina Grande/PR</p>
+            <p>Gênero: Arte em forma de SERTANEJO</p>
+            <p></p>
+            <p>Ingressos</p>
+            <p>Camarote (open bar): R$ 150,00</p>
+            <p>Camarote: R$ 100,00</p>
+            <p>Front Stage:R$ 60,00</p>
+            <p>Pista: R$ 30,00</p>
+        </section>
+    )
+}
